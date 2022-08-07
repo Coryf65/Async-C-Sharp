5ad830ca31078372a63518b50c940dce1dc599fb
Cory Fabian <Coryf65@users.noreply.github.com>
2022-08-07 13:46:41 -0500

    Simple threading example

---
+++ Threads/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("------------------ Threads and threading example ------------------");
+
+Console.WriteLine("Strating a thread");
+
+// Here we are not controlling when the threwads are starting and ending
+var t = new Thread(new ThreadStart(DoWork));
+t.Start();
+
+Console.WriteLine("Done...");
+
+void DoWork()
+{
+    Console.Write("Sleeping");
+    Console.Write(".");
+    Console.Write(".");
+    Console.Write(".");
+    Thread.Sleep(1000);
+}

115735e2c0878ac2703d6e1fdf49672ba487a4d9
Cory Fabian <Coryf65@users.noreply.github.com>
2022-08-07 15:02:27 -0500

    displaying more info about many threads

--- README.md
+++ README.md
@@ -14,5 +14,13 @@
 	
 3. Multithreading, Hard way
 
-	- Threads
-		()
+	- 
+
+### What is dangerous about threading?
+
+- Shared and / or mutable state
+	- two different threads trying to do the same task or memory
+- resulting locks and deadlocks, when using a resource to use exclusive access to something
+- seek out patterns and models to avoid deadlocks
+- "Thread Safe" data types can help
+
--- Threads/Program.cs
+++ Threads/Program.cs
@@ -4,16 +4,19 @@ Console.WriteLine("------------------ Threads and threading example ------------
 Console.WriteLine("Strating a thread");
 
 // Here we are not controlling when the threwads are starting and ending
-var t = new Thread(new ThreadStart(DoWork));
-t.Start();
+var t1 = new Thread(new ThreadStart(DoWork));
+t1.Start();
+
+var t2 = new Thread(new ThreadStart(DoWork));
+t2.Start();
+
+var t3 = new Thread(new ThreadStart(DoWork));
+t3.Start();
 
 Console.WriteLine("Done...");
 
-void DoWork()
+static void DoWork()
 {
-    Console.Write("Sleeping");
-    Console.Write(".");
-    Console.Write(".");
-    Console.Write(".");
-    Thread.Sleep(1000);
+    int x = 10;
+    Console.WriteLine("Doing Work for thread ID: {0} value: {1}", Thread.CurrentThread.ManagedThreadId, x);
 }

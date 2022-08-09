285f35732da88f5b238bcb4cb81f166490aadfed
Cory Fabian <Coryf65@users.noreply.github.com>
2022-08-08 20:12:23 -0500

    Displaying how to use Lock()

---
+++ Threads/LockKeyword.cs
@@ -0,0 +1,56 @@
+﻿namespace Threads
+{
+    internal static class LockKeyword
+    {
+
+        public static void DemoTwo()
+        {
+            Console.WriteLine("------------------ Lock Keyword example ------------------");
+
+            Console.WriteLine("demoing Lock() and it allows only one thread at a time");
+
+            var t1 = new Thread(new ThreadStart(AddToListLock));
+            Console.WriteLine("Starting a thread");
+            t1.Start();
+
+            var t2 = new Thread(new ThreadStart(AddToListLock));
+            Console.WriteLine("Starting a thread");
+            t2.Start();
+
+            var t3 = new Thread(new ThreadStart(AddToListLock));
+            Console.WriteLine("Starting a thread");
+            t3.Start();
+
+            Console.WriteLine("Done...");
+        }
+
+        static List<int> numbers = new();
+        private static object myLock = new();
+        private static Random rnd = new();
+
+        static void AddToListError()
+        {
+            // Example of an issue where many threads accessing a shared state can cause issues
+            for (int i = 0; i < 100; i++)
+            {
+                numbers.Add(i);
+            }
+        }
+
+        static void AddToListLock()
+        {
+            // lock makes it so only one thread at a time can run this code
+            // Flag inside the (), can be any atomic types
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(rnd.Next(1, 100));
+                lock (myLock)
+                {
+                    numbers.Add(i);
+                }
+            }
+            // You can see that at different times we have different results
+            numbers.ForEach(i => Console.Write("{0} ", i));
+        }
+    }
+}
--- Threads/Program.cs
+++ Threads/Program.cs
@@ -1,25 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Threads;
+
 Console.WriteLine("------------------ Threads and threading example ------------------");
 
-// Here we are not controlling when the threads are starting and ending
-// this means the can run in any order
-// Each thread will end whenever the thread is done with it's work
-var t1 = new Thread(new ThreadStart(DoWork));
-Console.WriteLine("Strating a thread");
-t1.Start();
+Console.WriteLine("Running Demo One, threading");
+DemoOne();
 
-var t2 = new Thread(new ThreadStart(DoWork));
-Console.WriteLine("Strating a thread");
-t2.Start();
-
-var t3 = new Thread(new ThreadStart(DoWork));
-Console.WriteLine("Strating a thread");
-t3.Start();
-
-Console.WriteLine("Done...");
+Console.WriteLine("Running Demo Two, threading and locks");
+LockKeyword.DemoTwo();
 
 static void DoWork()
 {
     int x = 10;
     Console.WriteLine("Doing Work for thread ID: {0} value: {1}", Thread.CurrentThread.ManagedThreadId, x);
+}
+
+static void DemoOne()
+{
+    // Here we are not controlling when the threads are starting and ending
+    // this means the can run in any order
+    // Each thread will end whenever the thread is done with it's work
+    var t1 = new Thread(new ThreadStart(DoWork));
+    Console.WriteLine("Starting a thread");
+    t1.Start();
+
+    var t2 = new Thread(new ThreadStart(DoWork));
+    Console.WriteLine("Starting a thread");
+    t2.Start();
+
+    var t3 = new Thread(new ThreadStart(DoWork));
+    Console.WriteLine("Starting a thread");
+    t3.Start();
+
+    Console.WriteLine("Done...");
 }

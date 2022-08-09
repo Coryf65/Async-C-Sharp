f4aca282d2411c1e18b4383068a9d44482aa6c36
Cory Fabian <Coryf65@users.noreply.github.com>
2022-08-08 23:14:35 -0500

    setting up an example for Async Networking with Tasks

---
+++ Networking/Task_Download.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace Networking
+{
+    public class Task_Download
+    {
+        string url = "https://www.twitch.tv";
+
+        [Fact]
+        public void Test_Download_MySite_async()
+        {
+            // Arrange
+            var httpRequestInfo = HttpWebRequest.CreateHttp(url);
+            // init round trip to server
+            var callback = new AsyncCallback(HttpResponseAvailable);
+            var ar = httpRequestInfo.BeginGetResponse(callback, httpRequestInfo);
+
+            ar.AsyncWaitHandle.WaitOne();
+        }
+
+        // callback method
+        private static void HttpResponseAvailable(IAsyncResult ar)
+        {
+            var httpRequestInfo = ar.AsyncState as HttpWebRequest;
+            var httpResponseInfo = httpRequestInfo.EndGetResponse(ar) as HttpWebResponse;
+
+            var responseStream = httpResponseInfo.GetResponseStream();
+            using (var sr = new StreamReader(responseStream))
+            {
+                string webPage = sr.ReadToEnd();
+            }
+        }
+    }
+}
